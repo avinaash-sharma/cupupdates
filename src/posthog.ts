26bbc9f470853bc547cf0e355cd6158e5b136032
Avinash <avi.rohit13@hotmail.com>
2026-04-03 17:12:17 +0530

    added posthog

--- src/posthog.ts
+++ src/posthog.ts
@@ -2,6 +2,6 @@ import Constants from 'expo-constants';
 import PostHog from 'posthog-react-native';
 
 const token: string = Constants.expoConfig?.extra?.posthogProjectToken ?? '';
-const host: string = Constants.expoConfig?.extra?.posthogHost ?? '';
+const host: string = Constants.expoConfig?.extra?.posthogHost ?? 'https://us.i.posthog.com';
 
-export const posthog = new PostHog(token, { host });
+export const posthog = new PostHog(token || 'unknown', { host, disabled: !token });
